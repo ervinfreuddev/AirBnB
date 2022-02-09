e004095b7781e82b530dc22e9af4adee1a6d8376
ervinfreuddev <ervinndemmeze@gmail.com>
2022-02-09 14:25:28 +0100

    date range picker

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,29 @@ import {
   UsersIcon,
   SearchIcon,
 } from "@heroicons/react/solid";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    {
+      setStartDate(ranges.selection.startDate);
+      setEndDate(ranges.selection.endDate);
+    }
+  };
+
   return (
     <header
       className="sticky top-0 z-50 grid 
@@ -32,6 +53,8 @@ function Header() {
             md:shadow-sm"
       >
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none text-sm
                   text-gray-600 placeholder-gray-400"
           type="text"
@@ -49,7 +72,7 @@ function Header() {
         className="flex items-center 
             space-x-4 justify-end text-gray-500"
       >
-        <p className="hidden md:inline cursor-pointer">become a hose </p>
+        <p className="hidden md:inline cursor-pointer">become a host</p>
         <GlobeAltIcon
           className="h-6 
                 cursor-pointer"
@@ -60,6 +83,16 @@ function Header() {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+      {searchInput && (
+        <div>
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+        </div>
+      )}
     </header>
   );
 }
